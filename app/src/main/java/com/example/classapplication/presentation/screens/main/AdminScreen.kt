17f877a8dc4ae096b8e1a5917e7cf39ba32d4fee
Muga20 <murimibrian18@outlook.com>
2024-01-23 11:37:14 +0300

    added comment

---
+++ app/src/main/java/com/example/classapplication/presentation/screens/main/AdminScreen.kt
@@ -0,0 +1,23 @@
+package com.example.classapplication.presentation.screens.main
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import com.example.classapplication.presentation.MainViewModel
+
+@Composable
+fun AdminScreen(navController: NavController, vm: MainViewModel) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(text = "Service Screen")
+
+
+        BottomNavigationMenu(
+            selectedItem = BottomNavigationItem.SERVICES,
+            navController = navController
+        )
+
+    }
+}
